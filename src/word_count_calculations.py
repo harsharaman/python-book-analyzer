984530c3b205c7a5f0f9e914f83259cadfe7a446
Harsha Raman <harsharaman97@gmail.com>
2024-01-09 17:29:25 +0100

    basic word counts

---
+++ src/word_count_calculations.py
@@ -0,0 +1,6 @@
+def get_number_of_total_words(words):
+    return len(words)
+
+
+def get_number_of_unique_words(words):
+    return len(set(words))
--- text_analyzer.py
+++ text_analyzer.py
@@ -1,8 +1,17 @@
 from src.utils import read_file
+from src.word_count_calculations import get_number_of_total_words, get_number_of_unique_words
 
 def main():
     text = read_file()
-    print(text)
+
+    words = text.split()
+    
+    number_of_words = get_number_of_total_words(words)
+    number_of_unique_words = get_number_of_unique_words(words)
+    
+    
+    print(f"Total number of words: {number_of_words}")
+    print(f"Total number of unique words: {number_of_unique_words}")
 
 if __name__ == '__main__':
     main()
